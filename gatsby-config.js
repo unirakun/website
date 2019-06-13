0721d41b44e82c6df5a48c78287f5c3e9024ad49
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2019-06-13 18:40:45 +0200

    :wrench: GA + CRISP

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'rakun',
+    title: 'unirakun',
     description: 'à deux, c’est mieux !',
-    author: '@rakun',
-    siteUrl: 'https://rakun.team',
+    author: '@unirakun',
+    siteUrl: 'https://unirakun.fr',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -48,14 +48,14 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-crisp-chat',
       options: {
-        websiteId: '989759e7-1622-453c-8749-1a8c08fb072e',
-        enableDuringDevelop: false,
+        websiteId: 'fef5442a-c7dd-421f-a62f-8836c9e2a665',
+        enableDuringDevelop: true,
       },
     },
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'UA-141280339-1',
+        trackingId: 'UA-142111529-1',
         anonymize: true,
         respectDNT: true,
       },
