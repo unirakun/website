8f561e8d3f89398e6c156c33bbc7693f573f74a4
Guillaume CRESPEL <quillaume.crespel@gmail.com>
2024-01-24 11:42:08 +0100

    :sparkles: Fabien / BeReal

---
+++ src/data/members/fabien/experiences/bereal.js
@@ -0,0 +1,40 @@
+module.exports = {
+  title: 'Lead Développeur Fullstack',
+  client: {
+    name: 'BeReal',
+    color: 'black',
+  },
+  for: {
+    name: 'unirakun',
+    color: 'white',
+  },
+  dates: {
+    from: Date.UTC(2022, 0),
+    to: Date.UTC(2024, 1),
+  },
+  informations: [
+    { text: '' },
+    { text: '' },
+    { text: '' },
+    { text: '' },
+    {
+      text: 'Missions :',
+      children: [
+        { text: '' },
+        { text: '' },
+        { text: '' },
+      ],
+    },
+    {
+      text: 'Technologies utilisées : ',
+      children: [
+        { text: '' },
+        { text: '' },
+        { text: '' },
+        { text: '' },
+        { text: '' },
+        { text: '' },
+      ],
+    },
+  ],
+}
